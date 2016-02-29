2e5bb765b2d49568ff44811082e0f758096223a4
Andreas Ahlin <Andreas Ahlin>
2016-02-29 14:15:51 +0100

    test

--- LibAppAndroid/app/src/main/java/com/example/andre/libappandroid/MainActivity.java
+++ LibAppAndroid/app/src/main/java/com/example/andre/libappandroid/MainActivity.java
@@ -12,10 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +43,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(this);
     }
 
     @Override
---
+++ LibAppAndroid/app/src/main/java/com/google/zxing/integration/android/IntentResult.java
@@ -0,0 +1,7 @@
+package com.google.zxing.integration.android;
+
+/**
+ * Created by Andre on 2016-02-29.
+ */
+public class IntentResult {
+}
